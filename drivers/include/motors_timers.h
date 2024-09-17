6112e82d5143d02e4e3d5fc2c4f1a228b55c087a
Jawad Mehmood Butt <70571986+Jawad-RoboLearn@users.noreply.github.com>
2024-09-17 14:29:33 +0800

    Update motors_timers.h

--- drivers/include/motors_timers.h
+++ drivers/include/motors_timers.h
@@ -8,9 +8,12 @@
 void motor_init(); // It can set up timer 2 ch1 for motor 1 and timer 2 ch2 for motor 2
 void set_M1_dir(int dir); // set motor 1 direction
 void set_M2_dir(int dir); // set motor 2 direction
-void set_M1_speed(uint32_t duty_cycle); // set motor 1 speed
-void set_M2_speed(uint32_t duty_cycle); // set motor 2 speed
-
+void set_M1_speed_duty(uint32_t duty_cycle); // set motor 1 speed
+void set_M2_speed_duty(uint32_t duty_cycle); // set motor 2 speed
+float get_M1_speed_counts (uint32_t duty_cycle); //get desired speed in counts
+float get_M2_speed_counts (uint32_t duty_cycle); //get desired speed in counts
+#define MAX_SPEED1_COUNT = 500;
+#define MAX_SPEED2_COUNT = 500;
 // ENCODER
 #define SYSCFG_EXTICR2_EXTI4       ((uint32_t)0x000F) // Mask for EXTI4
 #define SYSCFG_EXTICR2_EXTI4_PA    ((uint32_t)0x0000) // Connect EXTI4 to PA4
